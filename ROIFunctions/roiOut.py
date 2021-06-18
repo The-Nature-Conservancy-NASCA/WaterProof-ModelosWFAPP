fdd9cf556c0755e4e8763017adc0454a937014c3
Daniel Skaphe <daniel.parra@skaphe.com>
2021-06-17 23:36:28 -0500

    change order execution function

--- ROIFunctions/roiOut.py
+++ ROIFunctions/roiOut.py
@@ -17,12 +17,12 @@ CARBON = 'CARBONO'
 
 def SaveRoiDB( path_data, studycase ):
     anotherroute = path.join( path_data, OUT )
-    save.Save_roi( anotherroute, studycase, INTAKE, 'Cap', '6' )
-    save.Save_roi( anotherroute, studycase, PTAP, 'PTAP', '7' )
-    carb.Carb_roi( anotherroute, studycase )
-    sens.Sens_roi( anotherroute, studycase )
     cost.Cost_roi( anotherroute, studycase, IMPLEMENTATION, 'Implementation', '1' )
     cost.Cost_roi( anotherroute, studycase, MAINTENANCE, 'Maintenance', '2' )
     cost.Cost_roi( anotherroute, studycase, OPPORTUNITY, 'Opportunity', '3' )
     cost.Cost_roi( anotherroute, studycase, TRANSACTION,'Transaction', '4' )
     cost.Cost_roi( anotherroute, studycase, PLATFORM, 'Platform', '5' )
+    save.Save_roi( anotherroute, studycase, INTAKE, 'Cap', '6' )
+    save.Save_roi( anotherroute, studycase, PTAP, 'PTAP', '7' )
+    carb.Carb_roi( anotherroute, studycase )
+    sens.Sens_roi( anotherroute, studycase )
