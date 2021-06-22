dbcf8b75b39a1dd20b942fd6f96fd8880d64c62c
Daniel Skaphe <daniel.parra@skaphe.com>
2021-06-22 17:54:55 -0500

    get constants

--- ROIFunctions/roiOut.py
+++ ROIFunctions/roiOut.py
@@ -8,13 +8,13 @@ import constants
 ruta = environ["PATH_FILES"]
 
 def SaveRoiDB( path_data, studycase ):
-    anotherroute = path.join( path_data, OUT_ROI_DIR )
-    cost.Cost_roi( anotherroute, studycase, IMPLEMENTATION_ROI_DB, 'Implementation', '1' )
-    cost.Cost_roi( anotherroute, studycase, MAINTENANCE_ROI_DB, 'Maintenance', '2' )
-    cost.Cost_roi( anotherroute, studycase, OPPORTUNITY_ROI_DB, 'Opportunity', '3' )
-    cost.Cost_roi( anotherroute, studycase, TRANSACTION_ROI_DB,'Transaction', '4' )
-    cost.Cost_roi( anotherroute, studycase, PLATFORM_ROI_DB, 'Platform', '5' )
-    save.Save_roi( anotherroute, studycase, INTAKE_ROI_DB, 'Cap', '6' )
-    save.Save_roi( anotherroute, studycase, PTAP_ROI_DB, 'PTAP', '7' )
+    anotherroute = path.join( path_data, constants.OUT_ROI_DIR )
+    cost.Cost_roi( anotherroute, studycase, constants.IMPLEMENTATION_ROI_DB, 'Implementation', '1' )
+    cost.Cost_roi( anotherroute, studycase, constants.MAINTENANCE_ROI_DB, 'Maintenance', '2' )
+    cost.Cost_roi( anotherroute, studycase, constants.OPPORTUNITY_ROI_DB, 'Opportunity', '3' )
+    cost.Cost_roi( anotherroute, studycase, constants.TRANSACTION_ROI_DB,'Transaction', '4' )
+    cost.Cost_roi( anotherroute, studycase, constants.PLATFORM_ROI_DB, 'Platform', '5' )
+    save.Save_roi( anotherroute, studycase, constants.INTAKE_ROI_DB, 'Cap', '6' )
+    save.Save_roi( anotherroute, studycase, constants.PTAP_ROI_DB, 'PTAP', '7' )
     carb.Carb_roi( anotherroute, studycase )
     sens.Sens_roi( anotherroute, studycase )
