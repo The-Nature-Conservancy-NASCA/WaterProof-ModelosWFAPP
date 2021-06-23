2d4c67b483d5af2f936e5a5ec421bc96c0661b93
Daniel Skaphe <daniel.parra@skaphe.com>
2021-06-22 20:08:28 -0500

    fixed insert exchangeRateroi

--- ROIFunctions/exchangeRateROI.py
+++ ROIFunctions/exchangeRateROI.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from datetime import date
 from ROIFunctions.common_functions import insertParameter,getDataDB
-
+ 
 '''
 type_or_id: En este campo van los nombres de los costos o en el caso de la sbn, el id de la sbn
 cost: En este campo van los nombres de los costos, llamados del campo correspondiente a cada uno en la base de datos
@@ -31,28 +31,43 @@ def ExchangeROI(studyCase_id):
             "Others (Cost/yr)"]
     currency_db=[]
     res_first=[]
-
+    res_firstnbs=[]
+ 
     # Recorrer el resultado de las tasas de cambio dispobiles para un caso de estudio
     for res_rate in rate:
-
+ 
     # multiplicación dato carbón
         for res_carbon in carbon:
             res_carbon=list(res_carbon)
-            if(res_carbon[0]==res_rate[1]):
+            # validación de mismo currency (no convertir)
+            if(res_carbon[0] != res_rate[0]):
+                if(res_carbon[0]==res_rate[1]):
+                    res_first.append(res_carbon[1]/res_rate[2])
+                    currency_db.append(res_rate[0])
+                    type_or_id.append('Carbon')
+            else:
                 res_first.append(res_carbon[1])
                 currency_db.append(res_rate[0])
                 type_or_id.append('Carbon')
-
+ 
     # multiplicación dato financial  
         for res_finan in financial:
             res_finan=list( res_finan )
-            if(res_finan[0]==res_rate[1]):
+            # validación de mismo currency (no convertir)
+            if(res_finan[0] != res_rate[0]):
+                if(res_finan[0]==res_rate[1]):
+                    res_finan.pop(0)
+                    for sad in res_finan:
+                        res_first.append( sad/res_rate[2] )
+                        currency_db.append(res_rate[0])
+                        type_or_id.append('Financial')
+            else:
                 res_finan.pop(0)
                 for sad in res_finan:
                     res_first.append( sad )
                     currency_db.append(res_rate[0])
                     type_or_id.append('Financial')
-
+ 
     # multiplicación dato nbs
         for res_nbs in nbs:
             cost_nbs =["unit_implementation_cost","unit_maintenance_cost","unit_oportunity_cost"]
@@ -63,7 +78,7 @@ def ExchangeROI(studyCase_id):
                     curren=res_nbs.pop(4)
                     id_nbs=res_nbs.pop(0)
                     for idx, sad in enumerate(res_nbs):
-                        res_first.append( sad/res_rate[2] )
+                        res_firstnbs.append( sad/res_rate[2] )
                         currency_db.append(res_rate[0])
                         type_or_id.append(str(id_nbs))
                         cost.append(cost_nbs[idx])
@@ -73,19 +88,19 @@ def ExchangeROI(studyCase_id):
                 curren=res_nbs.pop(4)
                 id_nbs=res_nbs.pop(0)
                 for index, sad in enumerate(res_nbs):
-                    res_first.append( sad )
+                    res_firstnbs.append( sad )
                     currency_db.append(curren)
                     type_or_id.append(str(id_nbs))
                     cost.append(cost_nbs[index])
-
+ 
     # multiplicación con el Factor Multiplicador global
-    res_last=[]
-    for fin in res_first:
-        res_last.append(fin*fmultglo[0][0])
-    
+    res_lastnbs=[]
+    for fin in res_firstnbs:
+        res_lastnbs.append(fin*fmultglo[0][0])
+    res_last=res_first+res_lastnbs
     today = date.today()
     date_exec = today.strftime("%Y/%m/%d")
-
+    
     # ordenar la matriz de resultados para insertarlos en la DB
     final = pd.DataFrame(data=np.array([type_or_id,cost,res_last,currency_db]))
     for label,series in final.items():
@@ -95,4 +110,3 @@ def ExchangeROI(studyCase_id):
         ]
         # Insertar los resultados en la DB
         insertParameter( '__wp_roi_tc_insert', args )
-
