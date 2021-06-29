562b0b95adea44ca3fbc1ee6d271d0a88c4a2c1d
Edwin Alberto Piragauta Vargas <eapiragautav@dane.gov.co>
2021-06-29 12:31:38 -0500

    remove comment to execution command
    Signed-off-by: Edwin Alberto Piragauta Vargas <eapiragautav@dane.gov.co>

--- reclassify.py
+++ reclassify.py
@@ -107,12 +107,12 @@ def reclassify(pathFile,outPath,filename,lulc_path,json, is_future, future_lulc_
 
 def reclassifyFilesInFolder(path,lulc_path, is_future, future_lulc_path, year, region):
     print ("reclassifyFilesInFolder")
-    # print ("path : %s" % (path))
-    # print ("lulc_path : %s" % (lulc_path))
-    # print ("is_future %s" % (is_future))
-    # print ("future_lulc_path %s" % (future_lulc_path))
-    # print ("year: %s" % (year))
-    # print ("region: %s" % (region))
+    print ("path : %s" % (path))
+    print ("lulc_path : %s" % (lulc_path))
+    print ("is_future %s" % (is_future))
+    print ("future_lulc_path %s" % (future_lulc_path))
+    print ("year: %s" % (year))
+    print ("region: %s" % (region))
 
     pathOut = os.path.join(path,"translated_cob")
     json = readJsonActivities(path)
@@ -138,7 +138,7 @@ def reclassifyFilesInFolder(path,lulc_path, is_future, future_lulc_path, year, r
                 print ("lulc_path_complete : %s" % (lulc_path_complete))
                 command = "gdal_merge.py -o %s -of gtiff %s %s" % (lulc_path_complete, lulc_path_region, path_file)
                 print (command)
-                # print(os.popen(command).read())
+                print(os.popen(command).read())
             paths.append(path_file)
 
     return paths
