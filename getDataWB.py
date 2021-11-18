7b8e96b8527c50ce11799fb3c737374d24bb152d
Edwin Alberto Piragauta Vargas <edwin.piragauta@gmail.com>
2021-11-17 20:00:30 -0500

    debug getDataWB

--- getDataWB.py
+++ getDataWB.py
@@ -33,7 +33,7 @@ def generateCsvPerc(id, function_db, csv_in, path_data_wb_in):
 def generateCsvData(id, funcion_db, csv_in, path_data_in):
     print("generateCsvData: %s / %s" % (path_data_in, csv_in))
     results = getDataDB( [id], funcion_db )
-    print (:"results")
+    print ("results:")
     print (results)
     listElements = []
     listData = []
